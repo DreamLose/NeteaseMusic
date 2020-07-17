d42261317c0d90171b6d651f9e881da26cee1c43
DreamLose <1083531814@qq.com>
2020-07-17 17:50:44 +0800

    初始化项目

---
+++ NeteaseMusic/NeteaseMusic/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  NeteaseMusic
+//
+//  Created by 2020 on 2020/7/17.
+//  Copyright © 2020 2020. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+  
+
+
+}
+
